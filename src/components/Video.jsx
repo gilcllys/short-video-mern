b882d972bbd4855e0ab9c09b1c6f08577e7d64d0
gilcllys <gilcllyscosta@gmail.com>
2022-06-14 13:18:05 -0400

    upadte

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import styles from "./Video.module.css";
+import VideoFooter from "./VideoFooter";
 
 const Video = () => {
+  const [playing, setPlaying] = React.useState(false);
+  const videoRef = React.useRef(null);
+  const handleVideoPress = () => {
+    if (playing) {
+      videoRef.current.paused;
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.paused;
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
+
+
   return (
     <div className={styles.video}>
-      <video className={styles.videPlayer} controls>
-        <source src="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169738/video1_cvrjfm.mp4" type="video/mp4"/>
-      </video>
+      <video
+        src="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169738/video1_cvrjfm.mp4"
+        className={styles.videPlayer}
+        loop
+        ref={videoRef}
+        onClick={handleVideoPress}
+      ></video>
+      <VideoFooter />
     </div>
   );
 };
---
+++ src/components/VideoFooter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './VideoFooter.module.css'
+const VideoFooter = () => {
+  return (
+    <div className={styles.VideoFooter}>
+      <div className={styles.videoFooterText}>
+        <h3>@nabendu82</h3>
+        <p>Macbook Air to new Windows editing beast</p>
+      </div>
+    </div>
+  )
+}
+
+export default VideoFooter
