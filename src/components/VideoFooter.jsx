e0dc53e798d66db2eaaa420bb14e3be759c0c02e
gilcllys <gilcllyscosta@gmail.com>
2022-06-14 23:15:33 -0400

    adicionar icons

--- src/components/VideoFooter.jsx
+++ src/components/VideoFooter.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import styles from './VideoFooter.module.css'
+import MusicNoteIcon from '@mui/icons-material/MusicNote'
+
 const VideoFooter = () => {
   return (
     <div className={styles.VideoFooter}>
       <div className={styles.videoFooterText}>
         <h3>@nabendu82</h3>
         <p>Macbook Air to new Windows editing beast</p>
+        <div className={styles.videoFooterTicker}>
+          <MusicNoteIcon className={styles.videoFooterIcon} />
+        </div>
       </div>
     </div>
   )
